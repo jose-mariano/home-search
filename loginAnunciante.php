c33a9e74cc47ced75098e50bf6ce882ee3659e60
Larissa Lopes <lalopes2305@gmail.com>
2021-10-21 21:48:43 -0300

    Subindo tela de login e cadastro Anunciante

---
+++ loginAnunciante.php
@@ -0,0 +1,51 @@
+<!DOCTYPE HTML>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width-device-width, initial-scale-1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Controle de acesso</title>
+
+    <link rel="stylesheet" href="css/loginAnunciante.css">
+
+</head>
+
+<body>
+
+    <form class="form" action="#">
+        <div class="card">
+            <div class="card-top">
+                <img class="imglogin" src="img/login3.jpg" alt="">
+                <h2 class="title">Painel de Controle</h2>
+            </div>
+
+            <div class="card-group">
+                <label>CPF</label>
+            <input type="text" name="cpf" placeholder="Digite seu CPF" required>      
+            </div>
+
+            <div class="card-group">
+                <label>Senha</label>
+            <input type="password" name="cpf" placeholder="Digite sua senha" required>      
+            </div>
+
+            <div class="card-group">
+                <label><input type="checkbox">Lembre-me</label>     
+            </div>
+
+            <div class="card-group btn">
+                <button type="submit">ACESSAR</button>    
+            </div>
+
+            <div class="conta">
+                <p>Não tem uma conta? <a href="cadastroAnunciante.php">Cadastre-se agora</a></p>
+            </div>
+
+            <body link="grey">
+
+        </div>
+    </form>
+
+</body>
+
+</html>
